603c44b0f11675e07cdcca74297356eba7f2a7e1
Tirsie <tirzahstr@gmail.com>
2024-10-04 12:08:18 +0200

    accordion added

--- apps/web/app/page-2/page.tsx
+++ apps/web/app/page-2/page.tsx
@@ -1,8 +1,54 @@
 import { Button } from "@repo/ui/components/ui/button";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@repo/ui/components/ui/accordion"
+  
 
 export default function Home() {
     return (
-    <div className="buttonAccount">
-        <Button variant="destructive" >Join our cult</Button>
-    </div>  
-)}
+        <div>
+            <div>
+                <Accordion type="single" collapsible>
+
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-4">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                </Accordion>
+
+                <div className="buttonStyle">
+                    <Button variant="destructive">Button</Button>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
